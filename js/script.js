a7b2da5348e6b0483067043cf2838200cfa89cd9
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-07-17 23:56:37 +0600

    player one complete

--- js/script.js
+++ js/script.js
@@ -19,3 +19,21 @@ let chancenumbe2 = 0
 chance.innerHTML = chancenumber
 chance2.innerHTML = chancenumber
 
+// -------------------------------first person 
+
+const handlebutton = () => {
+  if (playroneinput.value == '') {
+    err.innerHTML = 'please enter your value'
+
+  } else if (playroneinput.value < 0 || playroneinput.value > 10) {
+    err.innerHTML = '0 theke 10 er moddhe value din'
+
+  } else {
+    err.innerHTML = ''
+    player1.style = 'display:none'
+    player2.style = 'display: block'
+    h2.innerHTML = 'player 2'
+    console.log(playroneinput.value)
+  }
+}
+
