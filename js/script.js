c92a34bec37a07a0e5ca6d673882b1a16fe0ca41
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-07-17 23:54:08 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let playroneinput = document.querySelector('.playrone')
+let err = document.querySelector('.err')
+let player1 = document.querySelector('.player1')
+let player2 = document.querySelector('.player2')
+let h2 = document.querySelector('h2')
+let playertwoinput = document.querySelector('.playertwoinput')
+let winnewr = document.querySelector('.winnewr')
+let h1 = document.querySelector('h1')
+let main_Box = document.querySelector('.main_Box')
+let playernumber = document.querySelector('.playernumber')
+let chance = document.querySelector('.chance')
+let player3 = document.querySelector('.player3')
+let playerthreeinput = document.querySelector('.playerthreeinput')
+let chance2 = document.querySelector('.chance2')
+
+let chancenumber = 0
+let chancenumbe2 = 0
+
+chance.innerHTML = chancenumber
+chance2.innerHTML = chancenumber
+
